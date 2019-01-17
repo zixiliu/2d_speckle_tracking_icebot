a4b0b66d63cabe64a5d97d019c3a208c9995b2a5
Zixi Liu <zeoliu2016@gmail.com>
2019-01-17 17:36:34 -0500

    demo example appears WORKING

--- demo_block_matching.py
+++ demo_block_matching.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from hog import hog_match
 
 
-def block_matching(template_gray, source_gray,  method=cv.TM_CCORR): 
+def block_matching(template_gray, source_gray,  method, savefilename): 
     if len(template_gray.shape) > 2 or len(source_gray.shape) > 2: 
         template_gray = cv.cvtColor(template_gray, cv.COLOR_BGR2GRAY) 
         source_gray = cv.cvtColor(source_gray, cv.COLOR_BGR2GRAY) 
@@ -23,6 +23,9 @@ def block_matching(template_gray, source_gray,  method=cv.TM_CCORR):
     thresh=0.8
     maxval = max(res.max(), thresh)
     loc = np.where( res >= maxval) 
+
+    y,x = loc[0][0], loc[1][0]
+    loc = [[y, x]]
     # pdb.set_trace()
     
     # # Draw a rectangle around the matched region. 
@@ -38,6 +41,37 @@ def block_matching(template_gray, source_gray,  method=cv.TM_CCORR):
     # plt.imshow(source_gray)
     # plt.show()
 
+    ## Draw all the blocks 
+    print(template_gray.shape, source_gray.shape)
+    template_width = template_gray.shape[0]
+    source_width = source_gray.shape[0]
+
+    temp = source_width - template_width + 1
+
+    print(loc)
+    print(res)
+    # plt.figure()
+    # plt.subplot(121)
+    # plt.imshow(template_gray, cmap='gray')
+    # plt.subplot(122)
+    # plt.imshow(source_gray, cmap='gray')
+    # plt.savefig(savefilename+'_1.png')
+
+
+    fig = plt.figure()
+
+    for i in range(temp * temp):
+        # plt.subplot(440+i+1)
+        fig.add_subplot(temp,temp,i+1)
+        subimg = source_gray[i/temp:i/temp+template_width, i%temp:i%temp+template_width]
+        plt.imshow(subimg, cmap='gray')
+    plt.savefig(savefilename+'_2.png')
+
+    # plt.show()
+
+    # pdb.set_trace()
+    
+
     return loc
 
 def find_match(source, template, next_img, prev_img, next_gray, prev_gray, ori_img, ori_prev, ds_factor, method, half_template_size, yy_ul, yy_lr, xx_ul, xx_lr, x,y, savefilename): 
@@ -49,7 +83,7 @@ def find_match(source, template, next_img, prev_img, next_gray, prev_gray, ori_i
     if method == 'hog': 
         loc = hog_match(template, source)
     else: 
-        loc = block_matching(template, source)
+        loc = block_matching(template, source, method, savefilename)
 
 
     next_img[x,y] = np.array([0,0,255]) 
@@ -63,18 +97,14 @@ def find_match(source, template, next_img, prev_img, next_gray, prev_gray, ori_i
         # print('new', (y-half_template_size, x-half_template_size),(y+half_template_size, x+half_template_size))
 
         # # Draw a rectangle around the matched region. 
-        
-        if method == 'hog': 
-            match_loc = loc
-        else: 
-            match_loc = zip(*loc[::-1])
+
         
         # print("match_loc", match_loc)
 
-        for pt in match_loc: 
+        for pt in loc: 
             
             match_ul_y, match_ul_x = pt[0], pt[1]
-            
+
             ## Matched block center in the previous frame
             # prev_match_y = y - half_source_size + half_template_size + match_ul_y
             # prev_match_x = x - half_source_size + half_template_size + match_ul_x
@@ -94,50 +124,73 @@ def find_match(source, template, next_img, prev_img, next_gray, prev_gray, ori_i
             # prev_img[prev_match_y, prev_match_x] = np.array([0,0,255])
             # print('prev', (y-half_source_size+match_ul_y, x-half_source_size+match_ul_x), (y-half_source_size+match_ul_y + w, x-half_source_size+match_ul_x + h)) 
             # cv.arrowedLine(next_img, (prev_match_y+1, prev_match_x+1), (y,x), (255,255,0), 1, tipLength=0.3)
-            
+           
             ## From previous frame to current frame
-            cv.line(next_img, (prev_match_y, prev_match_x), (y,x), (255,255,0), 1)
-            cv.arrowedLine(ori_img, (prev_match_y*ds_factor, prev_match_x*ds_factor), \
-                    (y*ds_factor,x*ds_factor), (255,255,0), 1, tipLength=0.3)
-            next_img[x,y] = np.array([255,0,0]) 
+            cv.arrowedLine(ori_img, (prev_match_x*ds_factor, prev_match_y*ds_factor), \
+                    (x*ds_factor,y*ds_factor), (255,255,0), 1, tipLength=0.3) 
+            cv.arrowedLine(ori_prev, (prev_match_x*ds_factor, prev_match_y*ds_factor), \
+                    (x*ds_factor,y*ds_factor), (255,255,0), 1, tipLength=0.3) 
 
-            cv.circle(ori_img, (y*ds_factor, x*ds_factor), 2, (255, 0, 0))
+            cv.circle(ori_img, (x*ds_factor, y*ds_factor), 2, (255, 0, 0))
+            cv.circle(ori_img, (prev_match_x*ds_factor, prev_match_y*ds_factor), 2, (0, 0, 255))
+            cv.circle(ori_prev, (x*ds_factor, y*ds_factor), 2, (255, 0, 0))
+            cv.circle(ori_prev, (prev_match_x*ds_factor, prev_match_y*ds_factor), 2, (0, 0, 255))
+            # cv.arrowedLine(ori_img, (prev_match_y*ds_factor, prev_match_x*ds_factor), \
+            #         (y*ds_factor,x*ds_factor), (255,255,0), 1, tipLength=0.3) 
+            # cv.arrowedLine(ori_prev, (prev_match_y*ds_factor, prev_match_x*ds_factor), \
+            #         (y*ds_factor,x*ds_factor), (255,255,0), 1, tipLength=0.3) 
+
+            # cv.circle(ori_img, (y*ds_factor, x*ds_factor), 2, (255, 0, 0))
+            # cv.circle(ori_img, (prev_match_y*ds_factor, prev_match_x*ds_factor), 2, (0, 0, 255))
+            # cv.circle(ori_prev, (y*ds_factor, x*ds_factor), 2, (255, 0, 0))
+            # cv.circle(ori_prev, (prev_match_y*ds_factor, prev_match_x*ds_factor), 2, (0, 0, 255))
+
             match_upper_left_y = (prev_match_y-half_template_size)*ds_factor
             match_upper_left_x = (prev_match_x-half_template_size)*ds_factor
             
             match_lower_right_y = (prev_match_y+half_template_size)*ds_factor
             match_lower_right_x = (prev_match_x+half_template_size)*ds_factor
             
-            cv.rectangle(ori_img, (match_upper_left_y,match_upper_left_x), (match_lower_right_y,match_lower_right_x), (0,0,255), 1)
-            cv.rectangle(ori_img, ((y-half_template_size)*ds_factor,(x-half_template_size)*ds_factor), ((y+half_template_size)*ds_factor,(x+half_template_size)*ds_factor), (255,0,0), 1) 
-            cv.rectangle(ori_prev, (match_upper_left_y,match_upper_left_x), (match_lower_right_y,match_lower_right_x), (0,0,255), 1)
-            cv.rectangle(ori_prev, ((y-half_template_size)*ds_factor,(x-half_template_size)*ds_factor), ((y+half_template_size)*ds_factor,(x+half_template_size)*ds_factor), (255,0,0), 1) 
+            # cv.rectangle(ori_img, (match_upper_left_y,match_upper_left_x), (match_lower_right_y,match_lower_right_x), (0,0,255), 1)
+            # cv.rectangle(ori_img, ((y-half_template_size)*ds_factor,(x-half_template_size)*ds_factor), ((y+half_template_size)*ds_factor,(x+half_template_size)*ds_factor), (255,0,0), 1) 
+            # cv.rectangle(ori_prev, (match_upper_left_y,match_upper_left_x), (match_lower_right_y,match_lower_right_x), (0,0,255), 1)
+            # cv.rectangle(ori_prev, ((y-half_template_size)*ds_factor,(x-half_template_size)*ds_factor), ((y+half_template_size)*ds_factor,(x+half_template_size)*ds_factor), (255,0,0), 1) 
+            cv.rectangle(ori_img, (match_upper_left_x,match_upper_left_y), (match_lower_right_x,match_lower_right_y), (0,0,255), 1)
+            cv.rectangle(ori_img, ((x-half_template_size)*ds_factor,(y-half_template_size)*ds_factor), ((x+half_template_size)*ds_factor,(y+half_template_size)*ds_factor), (255,0,0), 1) 
+            cv.rectangle(ori_prev, (match_upper_left_x,match_upper_left_y), (match_lower_right_x,match_lower_right_y), (0,0,255), 1)
+            cv.rectangle(ori_prev, ((x-half_template_size)*ds_factor,(y-half_template_size)*ds_factor), ((x+half_template_size)*ds_factor,(y+half_template_size)*ds_factor), (255,0,0), 1) 
             
-            ori_img[x*ds_factor,y*ds_factor] = np.array([255,0,0]) 
-            ori_prev[x*ds_factor,y*ds_factor] = np.array([255,0,0]) 
+            # ori_img[x*ds_factor,y*ds_factor] = np.array([255,0,0]) 
+            # ori_prev[x*ds_factor,y*ds_factor] = np.array([255,0,0]) 
 
-            ori_img[(x+match_ul_x)*ds_factor,(y+match_ul_y)*ds_factor] = np.array([0,0,255]) 
-            ori_prev[(x+match_ul_x)*ds_factor,(y+match_ul_y)*ds_factor] = np.array([0,0,255])
+            # ori_img[(x+match_ul_x)*ds_factor,(y+match_ul_y)*ds_factor] = np.array([0,0,255]) 
+            # ori_prev[(x+match_ul_x)*ds_factor,(y+match_ul_y)*ds_factor] = np.array([0,0,255])
 
-            cv.line(ori_img, (prev_match_y*ds_factor, prev_match_x*ds_factor), (y*ds_factor,x*ds_factor), (255,255,0), 1) 
-            cv.line(ori_prev, (prev_match_y*ds_factor, prev_match_x*ds_factor), (y*ds_factor,x*ds_factor), (255,255,0), 1) 
-
-            prev_block_ori = ori_prev[match_upper_left_x:match_lower_right_x,match_upper_left_y:match_lower_right_y]
-            next_block_ori = ori_img[(x-half_template_size)*ds_factor:(x+half_template_size)*ds_factor,\
-                                    (y-half_template_size)*ds_factor:(y+half_template_size)*ds_factor,:]
-            prev_block = source[match_ul_x:match_ul_x+w+1,match_ul_y:match_ul_y+w+1]
+            
+            prev_block_ori = ori_prev[match_upper_left_y:match_lower_right_y,match_upper_left_x:match_lower_right_x]
+            next_block_ori = ori_img[(y-half_template_size)*ds_factor:(y+half_template_size)*ds_factor,\
+                                        (x-half_template_size)*ds_factor:(x+half_template_size)*ds_factor,:]
+            # prev_block_ori = ori_prev[match_upper_left_x:match_lower_right_x,match_upper_left_y:match_lower_right_y]
+            # next_block_ori = ori_img[(x-half_template_size)*ds_factor:(x+half_template_size)*ds_factor,\
+            #                             (y-half_template_size)*ds_factor:(y+half_template_size)*ds_factor,:]
+            prev_block = source[match_ul_y:match_ul_y+w+1, match_ul_x:match_ul_x+w+1]
             next_block = template
 
             prev_gray_cp =  prev_gray.copy()
             next_gray_cp =  next_gray.copy()
 
-            cv.rectangle(prev_gray_cp, (y-half_template_size+match_ul_y, x-half_template_size+match_ul_x),\
-                                    (y+half_template_size+match_ul_y,x+half_template_size+match_ul_x), 255)
-            cv.rectangle(next_gray_cp, (y-half_template_size, x-half_template_size),(y+half_template_size,x+half_template_size), 255)
+            # cv.rectangle(prev_gray_cp, (prev_match_y-half_template_size, prev_match_x-half_template_size),\
+            #                             (prev_match_y+half_template_size, prev_match_x+half_template_size), 255)
+            # cv.rectangle(next_gray_cp, (y-half_template_size, x-half_template_size),(y+half_template_size,x+half_template_size), 255)
+            cv.rectangle(prev_gray_cp, (prev_match_x-half_template_size, prev_match_y-half_template_size),\
+                                        (prev_match_x+half_template_size, prev_match_y+half_template_size), 255)
+            cv.rectangle(next_gray_cp, (x-half_template_size, y-half_template_size),(x+half_template_size,y+half_template_size), 255)
 
 
-            cv.rectangle(ori_img, (yy_ul*ds_factor, xx_ul*ds_factor), (yy_lr*ds_factor, xx_lr*ds_factor), (0,255,0), 1)
-            cv.rectangle(ori_prev, (yy_ul*ds_factor, xx_ul*ds_factor), (yy_lr*ds_factor, xx_lr*ds_factor), (0,255,0), 1)
+            # cv.rectangle(ori_img, (yy_ul*ds_factor, xx_ul*ds_factor), (yy_lr*ds_factor, xx_lr*ds_factor), (0,255,0), 1)
+            # cv.rectangle(ori_prev, (yy_ul*ds_factor, xx_ul*ds_factor), (yy_lr*ds_factor, xx_lr*ds_factor), (0,255,0), 1)
+            cv.rectangle(ori_img, (xx_ul*ds_factor, yy_ul*ds_factor), (xx_lr*ds_factor, yy_lr*ds_factor), (0,255,0), 1)
+            cv.rectangle(ori_prev, (xx_ul*ds_factor, yy_ul*ds_factor), (xx_lr*ds_factor, yy_lr*ds_factor), (0,255,0), 1)
 
 
     plt.figure(figsize=(32,15))
@@ -211,7 +264,7 @@ def plot_match(prev_file, next_file, ori_prev, ori_file, ds_factor, savefilename
     ori_prev = cv.imread(ori_prev)
 
     ## define template by around a peak 
-    x, y = int(24*8./ds_factor), int(26*8./ds_factor)
+    x, y = int(25*8./ds_factor), int(26*8./ds_factor)
 
     # pdb.set_trace()
 
@@ -233,7 +286,7 @@ def plot_match(prev_file, next_file, ori_prev, ori_file, ds_factor, savefilename
     
     
     (x, y, prev_match_x, prev_match_y) = find_match(source, template, next_img, prev_img, next_gray, prev_gray, \
-                                        ori_img, ori_prev, ds_factor, method, half_source_size,\
+                                        ori_img, ori_prev, ds_factor, method, half_template_size,\
                                         yy_ul, yy_lr, xx_ul, xx_lr, x,y, savefilename)
 
     return (x, y, prev_match_x, prev_match_y)
@@ -307,7 +360,7 @@ def match_in_region(prev_file, next_file, ori_prev, ori_file, ds_factor, x, y, p
 
 
 
-method = 'hog'# cv.TM_CCORR
+method = cv.TM_CCORR_NORMED#cv.TM_CCORR #'hog'# cv.TM_CCORR
 
 half_template_size = 4
 
@@ -337,8 +390,10 @@ ori2 = ori_path+f2
 
 if method == 'hog': 
     savefilename = savefilepath+'HOG/'+'step0_down_by_8.png'
-else:
+elif method == cv.TM_CCORR:
     savefilename = savefilepath+'TM_CCORR/'+'step0_down_by_8.png'
+elif method == cv.TM_CCORR_NORMED:
+    savefilename = savefilepath+'TM_CCORR_NORMED/'+'step0_down_by_8.png'
 
 (x, y, prev_match_x, prev_match_y) = plot_match(file1, file2, ori1, ori2, 8, savefilename, half_template_size = half_template_size, half_source_size=6, method=method)
 # x, y = 48/2, 52/2
@@ -357,10 +412,12 @@ file2 = path+f2
 
 if method == 'hog': 
     savefilename = savefilepath+'HOG/'+'step1_down_by_4.png'
-else:
+elif method == cv.TM_CCORR:
     savefilename = savefilepath+'TM_CCORR/'+'step1_down_by_4.png'
+elif method == cv.TM_CCORR_NORMED:
+    savefilename = savefilepath+'TM_CCORR_NORMED/'+'step1_down_by_4.png'
 
-(x, y, prev_match_x, prev_match_y) = match_in_region(file1, file2, ori1, ori2, 4, x, y, prev_match_x, prev_match_y,8, savefilename, half_template_size = half_template_size, method =method)# 
+# (x, y, prev_match_x, prev_match_y) = match_in_region(file1, file2, ori1, ori2, 4, x, y, prev_match_x, prev_match_y,8, savefilename, half_template_size = half_template_size, method =method)# 
 # (x, y, prev_match_x, prev_match_y) = plot_match(file1, file2, ori1, ori2, 4, savefilename, half_template_size = half_template_size, method=method)
 # x, y = 48, 52
 # (x, y, prev_match_x, prev_match_y) = match_in_region(file1, file2, ori1, ori2, 4, x, y, x, y, 4, savefilename, half_template_size = half_template_size, method =method)# 
@@ -378,17 +435,22 @@ file2 = path+f2
 
 if method == 'hog': 
     savefilename = savefilepath+'HOG/'+'step2_down_by_2.png'
-else:
+elif method == cv.TM_CCORR:
     savefilename = savefilepath+'TM_CCORR/'+'step2_down_by_2.png'
+elif method == cv.TM_CCORR_NORMED:
+    savefilename = savefilepath+'TM_CCORR_NORMED/'+'step2_down_by_2.png'
 
-(x, y, prev_match_x, prev_match_y) = match_in_region(file1, file2, ori1, ori2, 2, x, y, prev_match_x, prev_match_y,4, savefilename, half_template_size = half_template_size, method =method)# 
+# (x, y, prev_match_x, prev_match_y) = match_in_region(file1, file2, ori1, ori2, 2, x, y, prev_match_x, prev_match_y,4, savefilename, half_template_size = half_template_size, method =method)# 
+(x, y, prev_match_x, prev_match_y) = match_in_region(file1, file2, ori1, ori2, 2, x, y, prev_match_x, prev_match_y,8, savefilename, half_template_size = half_template_size, method =method)# 
 
 
 if method == 'hog': 
-    savefilename = savefilepath+'HOG/'+'step3_final_step.png'
-else:
-    savefilename = savefilepath+'TM_CCORR/'+'step3_final_step.png'
-    
+    savefilename = savefilepath+'HOG/'+'step3_ori.png'
+elif method == cv.TM_CCORR:
+    savefilename = savefilepath+'TM_CCORR/'+'step3_ori.png'
+elif method == cv.TM_CCORR_NORMED:
+    savefilename = savefilepath+'TM_CCORR_NORMED/'+'step3_ori.png'
+
 (x, y, prev_match_x, prev_match_y) = match_in_region(ori1, ori2, ori1, ori2, 1, x, y, prev_match_x, prev_match_y,2, savefilename, half_template_size = half_template_size, method =method)
 
 
