d33ed12d5668c0520329e3db194b67880de0e3c9
Zixi Liu <zeoliu2016@gmail.com>
2019-10-02 23:01:39 -0400

    modify make movie

--- make_movie.py
+++ make_movie.py
@@ -4,13 +4,22 @@ import pdb
 import glob
 import os
 
-# file_path = 'images/original/'
-file_path = 'images/gaussian_blur/no warp no gaussian/'
-files = glob.glob(file_path+'*.jpg')
-files = sorted(files)
+
+def jpg_to_mp4(file_path, video_name):
+    files = glob.glob(file_path+'*.jpg')
+    files = sorted(files)
+
+    clips = [ImageClip(m).set_duration(0.05) for m in files] #0.2
+    # pdb.set_trace()
+    concat_clip = concatenate_videoclips(clips, method="compose")
+    concat_clip.write_videofile(video_name, fps=30)#fps=24)
 
 
-clips = [ImageClip(m).set_duration(0.05) for m in files] #0.2
-# pdb.set_trace()
-concat_clip = concatenate_videoclips(clips, method="compose")
-concat_clip.write_videofile("images/gaussian_blur/no warp no gaussian/no_warp_gaussian.mp4", fps=24)#fps=24)
+if __name__ == "__main__":
+    file_path = '/Users/zixiliu/Documents/Daichi/20190926_Images/20190621104753_S8_probe/tracking/'
+    video_file_name = '/Users/zixiliu/Documents/Daichi/20190926_Images/20190621104753_S8_probe/tracking/tracked.mp4'
+
+    jpg_to_mp4(file_path, video_file_name)
+
+
+
